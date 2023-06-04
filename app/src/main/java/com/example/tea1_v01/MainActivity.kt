44ca36629950ee73f51e8e588775804385521c68
VVAVANE <evan.rolland@centrale.centralelille.fr>
2023-06-04 10:20:32 +0200

    Ajout de messages Logcat
    Le bouton parametre ouvre une activité vide
    
    Un champ de texte pseudo et un bouton Ok
    
    Le bouton Ok enregistre le pseudo dans une liste pseudoList

--- app/src/main/java/com/example/tea1_v01/MainActivity.kt
+++ app/src/main/java/com/example/tea1_v01/MainActivity.kt
@@ -5,12 +5,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toolbar: Toolbar
+    private lateinit var editTextPseudo: EditText
+    private lateinit var buttonOk: Button
+    private val pseudoList: ArrayList<String> = ArrayList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,17 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.apply {
             //setDisplayHomeAsUpEnabled(true)
             //setHomeAsUpIndicator(R.drawable.settings_account_box_fill0_wght400_grad0_opsz48)
+        }
+
+        editTextPseudo = findViewById(R.id.editTextPseudo)
+        buttonOk = findViewById(R.id.buttonOk)
+
+        buttonOk.setOnClickListener {
+            val pseudo = editTextPseudo.text.toString()
+            pseudoList.add(pseudo)
+            editTextPseudo.clearComposingText()
+            // Faire quelque chose avec le pseudo, par exemple l'afficher dans le Logcat
+            Log.d("MainActivity", "Pseudo: $pseudo")
         }
     }
 
