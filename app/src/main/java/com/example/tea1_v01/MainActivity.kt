6dfba49dc7d23046dffa1961b0532ff6e0803fd3
VVAVANE <evan.rolland@centrale.centralelille.fr>
2023-06-04 09:38:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tea1_v01/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.tea1_v01
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        toolbar = findViewById(R.id.toolbarParametres)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            //setDisplayHomeAsUpEnabled(true)
+            //setHomeAsUpIndicator(R.drawable.settings_account_box_fill0_wght400_grad0_opsz48)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                // Action à effectuer lorsque le bouton des paramètres est cliqué
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+
+
+
+}
