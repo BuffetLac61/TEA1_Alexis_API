704b74ad7a893f9e87a64c6045c5f3c5b6226170
VVAVANE <evan.rolland@centrale.centralelille.fr>
2023-06-04 13:05:30 +0200

    Mise en place du paramètre pour supprimer la liste de pseudo
    Le bouton parametre ouvre une activité vide
    
    Un champ de texte pseudo et un bouton Ok
    
    Le bouton Ok enregistre le pseudo dans une liste pseudoList
    
    La liste est sauvegardée dans les paramètres partagés et est chargée à chaque lancement d'app
    
    Le bouton Ok lance aussi l'activité ChoixListActivity, qui est vide

--- app/src/main/java/com/example/tea1_v01/SettingsActivity.kt
+++ app/src/main/java/com/example/tea1_v01/SettingsActivity.kt
@@ -1,11 +1,29 @@
 package com.example.tea1_v01
 
 import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : PreferenceActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        //On affiche le xml root_preferences dans le conteneur qui a comme id "setting"
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+
+    }
+
 }
