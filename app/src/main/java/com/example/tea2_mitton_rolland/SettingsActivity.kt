85616ac46331d05d2f631702451779e12264bebb
Guilhem Mitton <guilhem.mitton@centrale.centralelille.fr>
2023-06-10 18:36:25 +0200

    Bouton d'URL de l'API dans les paramètres

--- app/src/main/java/com/example/tea2_mitton_rolland/SettingsActivity.kt
+++ app/src/main/java/com/example/tea2_mitton_rolland/SettingsActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
+import androidx.preference.EditTextPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
@@ -37,6 +38,19 @@ class SettingsActivity : PreferenceActivity() {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
             sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+
+            val apiUrlPreference = findPreference<EditTextPreference>("apiUrlInput")
+            apiUrlPreference?.setOnPreferenceChangeListener { preference, newValue ->
+                // Vérifier si la clé correspond à notre EditTextPreference
+                if (preference.key == "apiUrlInput") {
+                    // Vérifier si l'utilisateur a appuyé sur le bouton "OK"
+                    if (newValue is String) {
+                        Toast.makeText(context, "Mise à jour de l'URL de l'API", Toast.LENGTH_SHORT).show()
+                        sharedPreferences.edit().putString("apiUrl", newValue).apply()
+                    }
+                }
+                true
+            }
 
         }
 
@@ -82,8 +96,8 @@ class SettingsActivity : PreferenceActivity() {
 
 
 
-
     }
+
 
 
 }
